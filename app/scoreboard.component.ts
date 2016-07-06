06e99ab866fca87a41ee983812238d9abb2536d8
Adam Rensel <adamrensel@gmail.com>
2016-07-06 16:51:09 -0400

    Add wip scoreboard component

---
+++ app/scoreboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scoreboard',
+  template: `
+<header class="container">
+  <h1>Scoreboard</h1>
+</header>
+`
+})
+export class ScoreboardComponent {
+
+}
