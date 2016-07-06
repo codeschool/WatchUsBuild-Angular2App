96a044b4a123346e5973f88ca87e1212ab00f16a
Adam Rensel <adamrensel@gmail.com>
2016-07-06 16:51:19 -0400

    Add wip about component

---
+++ app/about.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  template: `
+<header class="container">
+  <h1>Something about races</h1>
+  
+  <p>Races are usually pretty awesome, schedule to race today!</p>
+</header>
+`
+})
+export class AboutComponent {
+
+}
