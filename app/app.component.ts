2670fc63d119f221a7855207f1e4221a240019de
Adam Rensel <adamrensel@gmail.com>
2016-07-06 16:51:57 -0400

    Rework the original app component to contain the router outlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
-import { RacesComponent } from './races.component';
 import { RaceService } from './race.service';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   selector: 'racing-app',
-  template: `
-<header class="container">
-  <h1>{{heading}}</h1>
-</header>
-<my-races></my-races>
+  template: ` 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a [routerLink]="['']">About</a></li>
+        <li><a [routerLink]="['/schedule']">Schedule</a></li>
+        <li><a [routerLink]="['/scoreboard']">Scoreboard</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<main class="container" role="main">
+  <router-outlet></router-outlet>
+<main>
 `,
-  directives: [RacesComponent],
-  providers:[RaceService, HTTP_PROVIDERS]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [RaceService, HTTP_PROVIDERS]
 })
 export class AppComponent {
-  heading = "Ultra Racing Schedule"
+
 }
